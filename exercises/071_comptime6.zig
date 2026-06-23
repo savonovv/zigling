b5510ac6a2a468b59c4aa6401aa1d397372798fd
savonovv <46849258+savonovv@users.noreply.github.com>
2026-06-23 15:50:03 +0400

    comptime next

--- exercises/071_comptime6.zig
+++ exercises/071_comptime6.zig
@@ -41,7 +41,7 @@ pub fn main() void {
     const field_names = @typeInfo(Narcissus).@"struct".field_names;
     const field_types = @typeInfo(Narcissus).@"struct".field_types;
 
-    ??? {
+    inline for (field_types) |field_type| {
         if (field_type != void) {
             print(" {s}", .{field_name});
         }
